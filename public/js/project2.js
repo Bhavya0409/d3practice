9caaa1598481ba60c9f872f14c5ffe295bbb52ba
Bhavya Shah <shahb@thingee.com>
2018-07-24 10:53:43 -0400

    created working scatter plot over time graph

--- public/js/project2.js
+++ public/js/project2.js
@@ -3,8 +3,6 @@ const margin = {left: 80, right: 20, top: 50, bottom: 100};
 const width = 800 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-const t = d3.transition().duration(750);
-
 const g = d3.select("#chart-area")
     .append('svg')
     .attr('width', width + margin.left + margin.right)
@@ -12,19 +10,22 @@ const g = d3.select("#chart-area")
     .append('g')
     .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-const xAxisGroup = g.append('g')
-    .attr('class', 'x-axis-label')
-    .attr('transform', 'translate(0, ' + height + ')');
+let time = 0;
 
-const yAxisGroup = g.append('g')
-    .attr('class', 'y-axis-label');
-
-const xScale = d3.scaleBand()
-    .range([0, width])
-    .padding(0.2);
+const xScale = d3.scaleLog()
+    .base(10)
+    .domain([300, 150000])
+    .range([0, width]);
 
 const yScale = d3.scaleLinear()
+    .domain([0, 90])
     .range([height, 0]);
+
+const area = d3.scaleLinear()
+    .domain([2000, 1400000000])
+    .range([25*Math.PI, 1500*Math.PI]);
+
+const continentColor = d3.scaleOrdinal(d3.schemePastel1);
 
 const xLabel = g.append('text')
     .text('GDP Per Capita ($)')
@@ -35,36 +36,77 @@ const xLabel = g.append('text')
 
 const yLabel = g.append('text')
     .text('Life Expectancy (Years)')
-    .attr('x', -height/2)
-    .attr('y', -60)
+    .attr('x', -170)
+    .attr('y', -40)
     .attr('text-anchor', 'middle')
     .attr('font-size', '20px')
     .attr('transform', 'rotate(-90)');
 
+const timeLabel = g.append("text")
+    .attr("y", height -10)
+    .attr("x", width - 40)
+    .attr("font-size", "40px")
+    .attr("opacity", "0.4")
+    .attr("text-anchor", "middle")
+    .text("1800");
+
+const xAxisCall = d3.axisBottom(xScale)
+    .tickValues([400, 4000, 40000])
+    .tickFormat(d3.format('$'));
+
+g.append('g')
+    .attr('class', 'x-axis')
+    .attr('transform', 'translate(0, ' + height + ')')
+    .call(xAxisCall);
+
+const yAxisCall = d3.axisLeft(yScale).tickFormat(d => +d);
+
+g.append('g')
+    .attr('class', 'y axis')
+    .call(yAxisCall);
+
 d3.json('data/gapminder.json').then((data) => {
 
-    console.log('here', data);
     const newData = data.map((yearObject) => {
         const newCountries = yearObject.countries.filter((country) => {
             return country.life_exp !== null && country.income !== null;
+        }).map((country) => {
+            country.income = +country.income;
+            country.life_exp = +country.life_exp;
+            return country;
         });
 
-        return {year: yearObject.year, countries: newCountries};
+        return {year: yearObject.year, countries: newCountries}
     });
 
-    console.log('after', newData);
+    console.log('new after', newData);
 
-    newData.forEach((d) => {
-       d.year = +d.year;
-    });
+    d3.interval(() => {
+        time = (time < 214) ? time+1 : 0;
+        update(newData[time].countries);
+    }, 100);
 
-    // update(data);
-
-    // d3.interval(() => {
-    //     update(data)
-    // }, 1000)
+    update(newData[0]);
 });
 
 function update(data) {
-    xScale.domain()
+    const t = d3.transition().duration(100);
+
+    const circles = g.selectAll('circle').data(data, d => d.country);
+
+    circles.exit()
+        .attr('class', 'exit')
+        .remove();
+
+    circles.enter()
+        .append('circle')
+        .attr('class', 'enter')
+        .attr('fill', d => continentColor(d.continent))
+        .merge(circles)
+        .transition(t)
+            .attr('cx', d => xScale(d.income))
+            .attr('cy', d => yScale(d.life_exp))
+            .attr('r', d => Math.sqrt(area(d.population) / Math.PI));
+
+    timeLabel.text(+(time + 1800))
 }
